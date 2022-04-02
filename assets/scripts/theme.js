c858904896ace4095a6f0e2e40aaa298f3c575ca
Chris <chris.d.fournier@gmail.com>
2022-04-02 08:52:14 -0400

    Replaced sessionStorage with localStorage

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -1,6 +1,6 @@
 const prefersColorScheme = window.matchMedia("(prefers-color-scheme: dark)");
 window.addEventListener("DOMContentLoaded", function(theme) {
-  if(sessionStorage.getItem("theme") === "false") {
+  if(localStorage.getItem("theme") === "false") {
     $("html").removeClass("dark");
     $("input#switch-theme").prop("checked",false);
   }
@@ -8,21 +8,21 @@ window.addEventListener("DOMContentLoaded", function(theme) {
     if(prefersColorScheme.matches) {
       $("html").toggleClass("dark");
       $("input#switch-theme").prop("checked",true);
-      sessionStorage.setItem("theme",true);
+      localStorage.setItem("theme",true);
     }
     else {
-      sessionStorage.setItem("theme",false);
+      localStorage.setItem("theme",false);
     }
   }
 });
 $("input#switch-theme").click(function() {
   $("html").toggleClass("dark");
-  if(sessionStorage.getItem("theme") === "true") {
+  if(localStorage.getItem("theme") === "true") {
     $("input#switch-theme").prop("checked",false);
-    sessionStorage.setItem("theme",false);
+    localStorage.setItem("theme",false);
   }
   else {
     $("input#switch-theme").prop("checked",true);
-    sessionStorage.setItem("theme",true);
+    localStorage.setItem("theme",true);
   }
 });
